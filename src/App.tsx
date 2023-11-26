adc75c922f7272f065a2a9249fd82b7aa080a9b8
John <55899462+JohnRsps77@users.noreply.github.com>
2023-11-26 00:27:39 -0800

    add pagination

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import BusinessDetailsPage from './components/BusinessDetailsPage';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import BusinessDetailsPage from "./components/BusinessDetailsPage";
 
 const App = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/api" element={
-        <BusinessDetailsPage />
-        } />
+        <Route path="/api" element={<BusinessDetailsPage />} />
       </Routes>
     </Router>
   );
